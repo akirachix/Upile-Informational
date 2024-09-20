7e8fa27837e52405d9929f14b88fa80c64fa9d07
Hannah-Unyolo <unyolohannah@gmail.com>
2024-09-20 06:06:10 +0300

    fixs media  queries

--- lost-locate/src/app/components/Footer/index.tsx
+++ lost-locate/src/app/components/Footer/index.tsx
@@ -11,7 +11,11 @@ const Footer = () => {
         <div className="bg-[#662113] text-white py-8 sm:py-12 md:py-16 lg:py-20 h-[500px] lg:h-[370px] md:h-[700px] lg:p-6 md:p-4">
             <div className=" flex flex-wrap md:block lg:flex justify-between items-start ml-8 lg:ml-12 md:ml-4">
                 <div className="w-full sm:w-1/4 text-center sm:text-left mb-6 sm:mb-0 lg:mt-12">
-                    <Image src='./media/lostlocate.png' alt='LostLocate Logo' className='w-32 h-auto sm:mx-0 lg:w-40 md:w-40' />
+                    <Image
+                     src='/media/lostlocate.png' 
+                     alt='LostLocate Logo' className='w-32 h-auto sm:mx-0 lg:w-40 md:w-40' 
+                     width={500}
+                     height={300}/>
                 </div>
                 <div className="w-full sm:w-1/4 text-justify sm:text-left mb-6 sm:mb-0 md:mt-12">
                     <h4 className="font-semibold text-[24px] sm:text-xl md:text-[28px] lg:text-3xl">Social Media</h4>
